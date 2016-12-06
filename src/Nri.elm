73f935ff70674e280300e8a2c40a12777e413659
Richard Feldman <richard.t.feldman@gmail.com>
2016-12-06 10:18:37 -0800

    :art: elm-format

--- src/Nri.elm
+++ src/Nri.elm
@@ -1,9 +1,12 @@
 module Nri exposing (name)
+
 {-| A collection of helpers for working with NoRedInk projects
 @docs name
 -}
 
+
 {-| NoRedInk's name!
 -}
 name : String
-name = "NoRedInk"
+name =
+    "NoRedInk"
--- src/Nri/Stylers.elm
+++ src/Nri/Stylers.elm
@@ -1,9 +1,11 @@
 module Nri.Stylers exposing (makeFont)
+
 {-| Helper module for working with fonts
 
 @docs makeFont
 
 -}
+
 import Css exposing (..)
 import Nri.Fonts
 
@@ -14,7 +16,7 @@ takes a size and a color and creates a font
 makeFont : Css.FontSize a -> Css.ColorValue b -> Mixin
 makeFont size fontColor =
     mixin
-        [  Nri.Fonts.baseFont
+        [ Nri.Fonts.baseFont
         , fontSize size
         , color fontColor
         ]
