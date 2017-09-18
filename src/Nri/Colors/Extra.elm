2b08ca12ee9b583f72f39441cfc634cd4fb6433c
SamGerber <gerbyxvi@gmail.com>
2017-09-18 11:04:54 -0700

    Move warning

--- src/Nri/Colors/Extra.elm
+++ src/Nri/Colors/Extra.elm
@@ -1,10 +1,6 @@
 module Nri.Colors.Extra exposing (..)
 
 {-| Helpers for working with colors. Put color definitions in Nri.Colors itself.
-**NOTE:** Rather than adding functions to convert css colors into their string
-representation, consider converting css colors to core colors and then using
-[elm-color-extra](http://package.elm-lang.org/packages/eskimoblood/elm-color-extra/5.0.0/)
-to convert to string.
 
 
 # Conversions
